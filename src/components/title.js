1a0348dee673775fbc3c67086acc8b7df783c8b5
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-13 18:06:38 -0700

    entering template component files

---
+++ src/components/title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Cover = () => {
+  return (
+    <div className= 'center-align align-wrapper'>
+
+      <div className='container'>
+        <div className='row'>
+          <h2 className='header col-lg-12 light'>Don't click on any image more than once!</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Cover;
